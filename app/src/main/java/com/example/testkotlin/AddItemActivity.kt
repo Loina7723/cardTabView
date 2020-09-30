d8134ec3fdab584d632c8e6097e83f4ceaa0bdca
odoo <Loina7723>
2020-09-30 19:53:50 +0800

    add item new page

--- app/src/main/java/com/example/testkotlin/AddItemActivity.kt
+++ app/src/main/java/com/example/testkotlin/AddItemActivity.kt
@@ -2,28 +2,34 @@ package com.example.testkotlin
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class AddItemActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cardview)
+        setContentView(R.layout.activity_add_item)
 
         val data = intent.extras
         val items = data?.getParcelableArrayList<CardData>("items")
 
-        val newData: ArrayList<String> = ArrayList()
-        newData.add("e")
-        newData.add("f")
-        items?.add(CardData("Third Cards", newData))
 
-        val button = findViewById<Button>(R.id.button_cv)
+        val button = findViewById<Button>(R.id.button_ai)
+        val additemEdittext = findViewById<EditText>(R.id.add_item_ai)
         button.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-            intent.putExtra("items", items)
-            startActivity(intent)
+            val newTab = additemEdittext.text.toString()
+            if(newTab.length > 0){
+                items?.add(CardData(newTab, ArrayList()))
+                val intent = Intent(this, MainActivity::class.java)
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                intent.putExtra("items", items)
+                startActivity(intent)
+            }
+            else
+                Toast.makeText(this, "Please enter new tab name", Toast.LENGTH_SHORT).show()
         }
     }
 }
